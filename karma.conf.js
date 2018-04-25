45faa56e6ac231c7542ce4e83e90dd4f0043e364
Derek Ross <derek.ross@saucelabs.com>
2018-04-24 17:35:19 -0700

    resetting demo

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,8 @@
 //jshint strict: false
-// Browsers to run on Sauce Labs
-var customLaunchers = {
-  'SL_Chrome': {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    version: '48.0',
-    platform: 'Linux'
-  },
-  'SL_Firefox': {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    version: '50.0',
-    platform: 'Windows 10'
-  },
-  'SL_InternetExplorer': {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    version: '11.0',
-    platform: 'Windows 7'
-  },
-  'SL_Safari': {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.11',
-    version: '10.0'
-  }
-};
-
 module.exports = function(config) {
   config.set({
 
-    basePath: './app/',
+    basePath: './app',
 
     files: [
       'bower_components/angular/angular.js',
@@ -46,37 +18,17 @@ module.exports = function(config) {
 
     browsers: ['Chrome'],
 
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
+
     junitReporter: {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
-    },
+    }
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'saucelabs'],
-
-    // web server port
-    port: 9876,
-
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    sauceLabs: {
-      testName: 'Pipeline Unit Test Demo',
-      startConnect: false,
-      tunnelIdentifier: 'derek_test_tunnel'
-    },
-    captureTimeout: 120000,
-    customLaunchers: customLaunchers,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: Object.keys(customLaunchers),
-    singleRun: true,
-  
   });
 };
